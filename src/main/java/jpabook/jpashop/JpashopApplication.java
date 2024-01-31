17fd04069fc44c09a6e4f2c349832fc7f91a4d4e
iamminseongKim <minseongkimim@gmail.com>
2024-01-31 16:17:42 +0900

    섹션 4 - 컬랙션 주문 조회 v2

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,6 +1,6 @@
 package jpabook.jpashop;
 
-import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -12,4 +12,8 @@ public class JpashopApplication {
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
+	@Bean
+	Hibernate5JakartaModule hibernate5Module() {
+		return new Hibernate5JakartaModule();
+	}
 }
